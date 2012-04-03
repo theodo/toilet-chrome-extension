92f1c8cc7c1be8b50aab791a5745aefb0c112d19
Benjamin Grandfond <benjaming@theodo.fr>
2012-04-03 20:32:08 +0200

    Add polling

--- toilet.js
+++ toilet.js
@@ -3,25 +3,61 @@ $(function () {
   var women = new Toilet($('#women'), 'captor2');
   var toilets = new Toilets(men , women);
   toilets.fetch();
+  toilets.poll();
 });
 
 (function () {
   var Toilets = function Toilets(men, women) {
     this.men = men;
     this.women = women;
+    this.intervalId = null;
+    this.delay = 3000;
+    this.url = 'http://lights.theodo.fr';
   };
 
   Toilets.prototype = {
     fetch: function () {
-      var url = 'http://lights.theodo.fr';
- 
-      $.get(url, $.proxy(function (data) {
+      $.get(this.url, $.proxy(function (data) {
         this.men.status = data[this.men.captor];
         this.men.update();
 
         this.women.status = data[this.women.captor];
         this.women.update();
+
+          this.updateIcon();
       }, this), 'json');
+    },
+
+    poll: function () {
+      if (null == this.intervalId) {
+        this.intervalId = setInterval($.proxy(this.fetch, this), this.delay);
+      }
+    },
+
+    stopPoll: function () {
+      if (null !== this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+    },
+
+    updateIcon: function () {
+      var iconPath = 'img/toilets_1.png';
+
+      if (this.women.isFree()) {
+        if (this.men.isFree()) {
+          iconPath = 'img/toilets_1.png'
+        } else {
+          iconPath = 'img/toilets_2.png'
+        }
+      } else {
+        if (this.men.isFree()) {
+          iconPath = 'img/toilets_3.png'
+        } else {
+          iconPath = 'img/toilets_4.png'
+        }
+      }
+
+      chrome.browserAction.setIcon({ path: iconPath });
     }
   };
 
